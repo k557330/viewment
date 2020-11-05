fb3e4ec1c473127cf97ff32fcfbbbe9592709752
JeongHyun <k557330@naver.com>
2020-11-05 14:33:42 +0900

    변수 이름 변경

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import useSelectAll from './hooks/useSelectAll';
 
 const App = () => {
-  const [host, setHost] = useSelectAll("test");
+  const [test, setTest] = useSelectAll("test");
 
 
   // useEffect(()=>setHost("test"), [setHost])
 
   return (
     <>
-      {console.log("host :", host)}
-      <div onClick={setHost}>API TEST</div>
+      {console.log(test)}
+      <div onClick={setTest}>API TEST</div>
     </>
   )
 }
